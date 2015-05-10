8cc3164b61264def4bed34c5515ee54eb5ed87d7
Mark Hurlburt <mark@primeacademy.io>
2015-05-10 00:38:14 -0500

    added sample twillio functionality

--- routes/bathrooms.js
+++ routes/bathrooms.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// Setup Database and Model Info
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/gottago');
 var Bathroom = mongoose.model('Bathroom', {name:String, occupied:Boolean});
+
+// Setup Twilio
+var accountSid = 'ACcaebeaad7593fb33f0c80283d25da632';
+var authToken = 'e3f30e916c7ef5c759f618d1f90db267';
+var twilio = require('twilio')(accountSid, authToken);
 
 /* GET bathrooms listing. */
 router.get('/', function(req, res, next) {
@@ -13,17 +19,33 @@ router.get('/', function(req, res, next) {
   })
 });
 
+// UPDATE bathroom occupancy
 router.post('/:id', function(req, res, next){
   bathroom = Bathroom.findOne({name:req.params.id},function(err,bathroom) {
     bathroom.occupied = req.body.occupied;
-    console.log(req);
     bathroom.save(function(err){
       if(err) throw new Error(err);
+      if(!bathroom.occupied) {
+        // TODO: Check if unoccupied and notify anyone in the queue
+        twilio.messages.create({
+            body: "Gotta go?",
+            to: "+16127039976",
+            from: "+19522227346"
+        }, function(err, message) {
+            if (err) {
+              process.stdout.write(err.message);
+            } else {
+              process.stdout.write(message.sid);
+            }
+        });        
+      }
+      // Return Bathroom in JSON
       res.json(bathroom);
     });
   });
 });
 
+// Method for creating bathrooms (stubbed out)
 router.post('/', function(request, response, next){
    var bathroom = new Bathroom({name: request.body.name});
    bathroom.save(function(err){
