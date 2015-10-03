d3ece9cde7b4e7e4ebe430055661b978d878e0cd
Mark Hurlburt <mark@primeacademy.io>
2015-10-03 14:32:40 -0500

    a bunch of changes - I'm a bad programmer

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 var express = require('express');
+var app = express();
+var server = require('http').Server(app);
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var io = require('socket.io')(server);
 
 var routes = require('./routes/index');
 var bathrooms = require('./routes/bathrooms');
-
-
-
-var app = express();
+app.use('/', routes);
+app.use('/bathrooms', bathrooms);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'))
 
-app.use('/', routes);
-app.use('/bathrooms', bathrooms);
+// Setup Database and Model Info
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./data/gottago.db');
+
+// Setup Particle.io communications
+var spark = require('spark');
+
+// Subscribe to bathroom notifications
+spark.on('login', function() {
+  spark.onEvent('bathroom', function(data) {
+    console.log(data);
+    // Sample data: {"data":"not_occupied","ttl":"60","published_at":"2015-09-02T04:31:18.577Z","coreid":"2a0036001347343339383037","name":"bathrooms/bit"}
+    var bathroom_name = data.name.substring(10);
+    var bathroom_status = data.data == "occupied" ? 1 : 0;
+    // Update DB to set Bathroom occupancy
+    db.run("UPDATE bathrooms SET occupied = ? WHERE name = ? ", bathroom_status, bathroom_name);
+    // Send updates to listening browsers
+    io.emit('occupancy_update', { bathroom: bathroom_name, occupied:bathroom_status });
+  });
+});
+
+// Login as usual
+var promise = spark.login({ username: 'mhurlburt@gmail.com', password: 'starwars' });
+
+// ERROR HANDLERS
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,8 +51,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
@@ -60,5 +74,4 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-module.exports = app;
+server.listen(process.env.PORT || '3000');
--- routes/bathrooms.js
+++ routes/bathrooms.js
@@ -1,50 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-// Setup Database and Model Info
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/gottago');
-var Bathroom = mongoose.model('Bathroom', {name:String, occupied:Boolean, description:String, men:Boolean, women:Boolean});
-
 // Setup Twilio
-var accountSid = 'ACcaebeaad7593fb33f0c80283d25da632';
-var authToken = 'e3f30e916c7ef5c759f618d1f90db267';
-var twilio = require('twilio')(accountSid, authToken);
+// var accountSid = 'ACcaebeaad7593fb33f0c80283d25da632';
+// var authToken = 'e3f30e916c7ef5c759f618d1f90db267';
+// var twilio = require('twilio')(accountSid, authToken);
+
+// Setup Database and Model Info
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./data/gottago.db');
+
+// Create Table if necessary
+// db.serialize(function() {
+//   db.run("CREATE TABLE if not exists bathrooms (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, occupied INTEGER)");
+//   var stmt = db.prepare("INSERT INTO bathrooms (name,occupied) VALUES (?,?)");
+//   for (var i = 1; i < 5; i++) {
+//       stmt.run("Bathroom " + i, 0);
+//   }
+//   stmt.finalize();
+// });
+
 
 /* GET bathrooms listing. */
 router.get('/', function(req, res, next) {
-  return Bathroom.find({},function(err,bathrooms){
+  db.all("SELECT * from bathrooms",function(err,bathrooms){
+    console.log(bathrooms);
     if(err) throw new Error(err);
     res.render('bathrooms', { bathrooms: bathrooms });
-    // res.json(bathrooms);
   })
 });
 
-// UPDATE bathroom occupancy
-router.put('/:id', function(req, res, next){
-  bathroom = Bathroom.findOne({name:req.params.id},function(err,bathroom) {
-    bathroom.occupied = req.body.occupied;
-    bathroom.save(function(err){
-      if(err) throw new Error(err);
-      if(!bathroom.occupied) {
-        // TODO: Check if unoccupied and notify anyone in the queue
-        // twilio.messages.create({
-        //     body: "Gotta go?",
-        //     to: "+16127039976",
-        //     from: "+19522227346"
-        // }, function(err, message) {
-        //     if (err) {
-        //       process.stdout.write(err.message);
-        //     } else {
-        //       process.stdout.write(message.sid);
-        //     }
-        // });
-      }
-      // Return Bathroom in JSON
-      res.json(bathroom);
-    });
-  });
-});
+// // UPDATE bathroom occupancy
+// router.put('/:id', function(req, res, next){
+//   console.log(req.body.occupied);
+//   db.run("UPDATE bathrooms SET occupied = ? WHERE id = ? ",req.body.occupied,req.params.id);
+//   res.redirect("/bathrooms");
+// });
 
 // Method for creating bathrooms (stubbed out)
 // router.post('/', function(request, response, next){
